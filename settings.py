4319f6e16f67ba8e13f09311c942211bd2392ac6
xarion <erdicalli@gmail.com>
2015-11-15 13:22:24 +0100

    added mongo configuration

--- settings.py
+++ settings.py
@@ -8,3 +8,7 @@ twitter_config['consumer_key'] = configParser.get('twitter', 'consumer_key')
 twitter_config['consumer_secret'] = configParser.get('twitter', 'consumer_secret')
 twitter_config['access_token'] = configParser.get('twitter', 'access_token')
 twitter_config['access_token_secret'] = configParser.get('twitter', 'access_token_secret')
+
+mongod_config = dict()
+mongod_config['connection_string'] = configParser.get('mongod', 'connection_string')
+mongod_config['database_name'] = configParser.get('mongod', 'database_name')
