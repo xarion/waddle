3eb83ecb35121ee70de9a6ed950e74d3e4613556
xarion <erdicalli@gmail.com>
2015-12-10 09:11:21 +0100

    moved filtering implementation to a function

--- reader.py
+++ reader.py
@@ -6,6 +6,7 @@ from helpers import ChunkedStream
 
 class TweetReader:
     def __init__(self, twitter_config):
+
         self.api = twitter.Api(consumer_key=twitter_config['consumer_key'],
                                consumer_secret=twitter_config['consumer_secret'],
                                access_token_key=twitter_config['access_token'],
@@ -20,12 +21,15 @@ class TweetReader:
         stream = self.get_sample_stream()
         for sample in stream:
             if 'contributors' in sample:  # this is a tweet
-                if sample['geo'] is not None \
-                        and sample['geo']['type'] is u"Point" \
-                        and sample['place']['country_code'] is "US"\
-                        and sample['lang'] is "en":
-                    print("yielding")
+                if TweetReader.filter(sample):
                     yield sample
 
     def get_user_timeline(self, user):
         return self.api.GetUserTimeline(user_id=user["id"], exclude_replies=True, count=40)
+
+    @staticmethod
+    def filter(sample):
+        return sample['geo'] is not None \
+               and sample['lang'] == u"en" \
+               and sample['geo']['type'] == u"Point" \
+               and sample['place']['country_code'] == u"US"
