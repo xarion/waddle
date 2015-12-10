b12f28a11fde1cf674b5d5d70bf509a5a2c2ff5a
xarion <erdicalli@gmail.com>
2015-12-10 09:12:06 +0100

    added mini wrapper to insert data better variable names.

--- data.py
+++ data.py
@@ -3,13 +3,16 @@ from pymongo import MongoClient
 
 
 class MongoDB:
-    tweets_collection = 'tweets'
+    tweets_collection_name = 'tweets'
 
     def __init__(self, mongodb_config):
         self.client = MongoClient(mongodb_config['connection_string'])
         self.db = self.client[mongodb_config['database_name']]
-        self.tweets_collection = self.db[MongoDB.tweets_collection]
+        self.tweets_collection = self.db[MongoDB.tweets_collection_name]
 
     def write_stream(self, stream):
         for batch in stream:
             self.tweets_collection.insert_many(batch)
+
+    def write(self, document):
+        self.tweets_collection.insert_one(document)
