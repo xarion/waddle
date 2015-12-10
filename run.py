e4ad043314ba46fb252d1671d1f1ac0bd0c0d705
xarion <erdicalli@gmail.com>
2015-12-10 09:13:05 +0100

    implemented data collection script

--- run.py
+++ run.py
@@ -6,4 +6,22 @@ from settings import twitter_config, mongod_config
 mongodb = MongoDB(mongod_config)
 reader = TweetReader(twitter_config)
 
-mongodb.write_stream(reader.get_sample_stream_with_location())
+
+def serialize_timeline_status(timeline_status):
+    return {"lang": timeline_status.lang,
+            "favorited": timeline_status.favorited,
+            "truncated": timeline_status.truncated,
+            "text": timeline_status.text,
+            "created_at": timeline_status.created_at,
+            "retweeted": timeline_status.retweeted,
+            "source": timeline_status.source,
+            "in_reply_to_screen_name": timeline_status.in_reply_to_screen_name,
+            "id": timeline_status.id,
+            "in_reply_to_user_id": timeline_status.in_reply_to_user_id
+            }
+
+
+for statuses in reader.get_sample_stream_with_location():
+    for status in statuses:
+        status["timeline"] = map(lambda t: serialize_timeline_status(t), reader.get_user_timeline(status['user']))
+        mongodb.write(status)
