54505c54628696c08f305e2f11554f0758a76fd2
xarion <erdicalli@gmail.com>
2016-01-04 11:26:44 +0100

    created class data to remove logic from analysis script

--- bayesian_analysis_no_cleanup.py
+++ bayesian_analysis_no_cleanup.py
@@ -1,42 +1,26 @@
 from analysis.EmpiricalBayes import EmpiricalBayes
-from data import MongoDB
+from data import MongoDB, Data
 from settings import mongod_config
 
-training_factor = 0.8
+training_factor = 0.6
 
 mongodb = MongoDB(mongod_config)
-docs = mongodb.tweets_collection.find()
-m = dict()
-count = 0
-for doc in docs:
-    l = doc['place']['full_name']
-    if l not in m:
-        m[l] = 0
-    m[l] += 1
-    count += 1
+data = Data(mongodb)
+location_filter_query = data.get_location_filter_query()
 
-for n in m.keys():
-    if m[n] < count * 0.01:
-        del m[n]
-
-query = []
-
-for n in m.keys():
-    query.append({"place.full_name": n})
-
-doc_count = mongodb.tweets_collection.find({"$or": query}).count()
+doc_count = mongodb.tweets_collection.find({"$or": location_filter_query}).count()
 
 training_doc_count = int(doc_count * training_factor)
 
-training_data = mongodb.tweets_collection.find({"$or": query}).limit(training_doc_count)
+training_data = mongodb.tweets_collection.find({"$or": location_filter_query}).limit(training_doc_count)
 
 bayes = EmpiricalBayes()
 bayes.train(training_data, training_doc_count)
 
-test_data = mongodb.tweets_collection.find({"$or": query}).skip(training_doc_count)
+test_data = mongodb.tweets_collection.find({"$or": location_filter_query}).skip(training_doc_count)
 collective_matches = bayes.test_collective(test_data)
 
-test_data = mongodb.tweets_collection.find({"$or": query}).skip(training_doc_count)
+test_data = mongodb.tweets_collection.find({"$or": location_filter_query}).skip(training_doc_count)
 singular_matches = bayes.test_singular(test_data)
 
 total_tested = doc_count - training_doc_count
@@ -45,7 +29,7 @@ print "collective precision: %f" % (float(collective_matches) / total_tested)
 
 print "singular matches: %d" % singular_matches
 print "singular precision: %f" % (float(singular_matches) / total_tested)
-# collective matches: 46
-# collective precision: 0.086466
-# singular matches: 46
-# singular precision: 0.086466
+# collective matches: 100
+# collective precision: 0.093985
+# singular matches: 100
+# singular precision: 0.093985
--- data.py
+++ data.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 from pymongo import MongoClient
 
+from helpers import Dictrement
+
 
 class MongoDB:
     tweets_collection_name = 'tweets'
@@ -16,3 +18,33 @@ class MongoDB:
 
     def write(self, document):
         self.tweets_collection.insert_one(document)
+
+
+class Data:
+    def __init__(self, mongodb):
+        self.mongodb = mongodb
+
+    def get_filtered_locations(self):
+        docs = self.mongodb.tweets_collection.find({}, {"place.full_name": 1, "_id": 0})
+        location_counts = Dictrement()
+        count = 0
+        for doc in docs:
+            location_fullname = doc['place']['full_name']
+            location_counts.increment(location_fullname)
+            count += 1
+
+        filtered_locations = location_counts.get_dict()
+        for n in filtered_locations.keys():
+            if filtered_locations[n] < count * 0.01:
+                del filtered_locations[n]
+
+        return filtered_locations
+
+    def get_location_filter_query(self):
+        query = []
+        for location_name in self.get_filtered_locations().keys():
+            query.append({"place.full_name": location_name})
+        return query
+
+    def get_training_data(self):
+        pass
