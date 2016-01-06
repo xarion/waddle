4bdeabf690753f973b9005b04304ee5d5d9526e9
xarion <erdicalli@gmail.com>
2016-01-06 11:40:30 +0100

    implemented new locations on filters

--- data/Filters.py
+++ data/Filters.py
@@ -1,4 +1,4 @@
-from helpers import Dictrement
+import settings
 
 
 class Filters:
@@ -6,25 +6,9 @@ class Filters:
         self.mongodb = mongodb
         self.source_whitelist = Filters.get_source_whitelist()
 
-    def get_filtered_locations(self):
-        docs = self.mongodb.tweets_collection.find({}, {"place.full_name": 1, "_id": 0})
-        location_counts = Dictrement()
-        count = 0
-        for doc in docs:
-            location_fullname = doc['place']['full_name']
-            location_counts.increment(location_fullname)
-            count += 1
-
-        filtered_locations = location_counts.get_dict()
-        for n in filtered_locations.keys():
-            if filtered_locations[n] < count * 0.01:
-                del filtered_locations[n]
-
-        return filtered_locations
-
     def get_location_filter_query(self):
         query = []
-        for location_name in self.get_filtered_locations().keys():
+        for location_name in settings.locations.keys():
             query.append({"place.full_name": location_name})
         return query
 
