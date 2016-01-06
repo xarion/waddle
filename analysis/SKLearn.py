301ef0ae357221b276e29ec106afb42caad9ebb0
xarion <erdicalli@gmail.com>
2016-01-06 11:42:15 +0100

    added sklearn and numpy to requirements implemented sklearn executor created script to specified executor.

---
+++ analysis/SKLearn.py
@@ -0,0 +1,41 @@
+import numpy
+from sklearn import svm
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+import settings
+
+
+class ClassifierExecutor:
+    def __init__(self, classifier):
+        self.classifier = svm.SVC(kernel='linear')
+
+    def execute(self, training, test):
+        self.classifier.fit(training['tfidfs'], training['document_labels'])
+        predicted = self.classifier.predict(test['tfidfs'])
+        return numpy.mean(predicted == test['document_labels'])
+
+
+class DataVectorizer:
+    def __init__(self, ngram_range=(1, 1)):
+        self.vectorizer = CountVectorizer(min_df=1, ngram_range=ngram_range)
+        self.tfidf_transformer = TfidfTransformer()
+
+    def convert(self, training_docs, test_docs):
+        training_corpus = self.__create_corpora__(training_docs)
+        test_corpus = self.__create_corpora__(test_docs)
+
+        training_corpus['tfidfs'] = self.tfidf_transformer.fit_transform(
+                self.vectorizer.fit_transform(training_corpus['documents']))
+        test_corpus['tfidfs'] = self.tfidf_transformer.transform(self.vectorizer.transform(test_corpus['documents']))
+        return {"training": training_corpus, "test": test_corpus}
+
+    def __create_corpora__(self, docs):
+        documents = []
+        document_labels = []
+        for doc in docs:
+            full_name = doc['place']['full_name']
+            document_label_index = settings.locations[full_name]
+            for tweet in doc['timeline']:
+                documents.append(tweet['text'])
+                document_labels.append(document_label_index)
+        return {"document_labels": document_labels, "documents": documents}
---
+++ sklearn_comparison.py
@@ -0,0 +1,172 @@
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.ensemble import GradientBoostingClassifier, AdaBoostClassifier
+from sklearn.gaussian_process import GaussianProcess
+from sklearn.linear_model import LogisticRegressionCV, SGDClassifier, RidgeCV, LinearRegression, LassoCV, ElasticNetCV, \
+    MultiTaskLassoCV, BayesianRidge, ARDRegression, Perceptron, PassiveAggressiveClassifier
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
+from sklearn.neighbors import KNeighborsClassifier, RadiusNeighborsClassifier
+from sklearn.svm import SVC, NuSVC, LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+
+from analysis.SKLearn import DataVectorizer, ClassifierExecutor
+from data.Data import Data
+
+classifiers = [
+    LinearRegression(),
+    RidgeCV(alphas=[.1, .5, 1, 5, 10]),
+    LassoCV(),
+    ElasticNetCV(),
+    MultiTaskLassoCV(),
+    BayesianRidge(),
+    ARDRegression(),
+    LogisticRegressionCV(),
+    SGDClassifier(loss="hinge"),
+    SGDClassifier(loss="log"),
+    SGDClassifier(loss="modified_huber"),
+    SGDClassifier(loss="squared_hinge"),
+    SGDClassifier(loss="perceptron"),
+    SGDClassifier(loss="squared_loss"),
+    SGDClassifier(loss="huber"),
+    SGDClassifier(loss="epsilon_insensitive"),
+    SGDClassifier(loss="squared_epsilon_insensitive"),
+    Perceptron(),
+    PassiveAggressiveClassifier(loss="hinge"),
+    PassiveAggressiveClassifier(loss="squared_hinge"),
+    LinearDiscriminantAnalysis(),
+    LinearDiscriminantAnalysis(solver="lsqr"),
+    LinearDiscriminantAnalysis(solver="eigen"),
+    SVC(kernel='poly'),
+    SVC(kernel='sigmoid'),
+    SVC(kernel='precomputed'),
+    NuSVC(),
+    LinearSVC(),
+    KNeighborsClassifier(n_neighbors=8, weights="uniform"),
+    KNeighborsClassifier(n_neighbors=8, weights="distance"),
+    RadiusNeighborsClassifier(weights="uniform"),
+    RadiusNeighborsClassifier(weights="distance"),
+    GaussianProcess(regr="constant", optimizer="fmin_cobyla", corr="absolute_exponential"),
+    GaussianProcess(regr="constant", optimizer="Welch", corr="absolute_exponential"),
+    GaussianProcess(regr="linear", optimizer="fmin_cobyla", corr="absolute_exponential"),
+    GaussianProcess(regr="linear", optimizer="Welch", corr="absolute_exponential"),
+    GaussianProcess(regr="quadratic", optimizer="fmin_cobyla", corr="absolute_exponential"),
+    GaussianProcess(regr="quadratic", optimizer="Welch", corr="absolute_exponential"),
+    GaussianProcess(regr="constant", optimizer="fmin_cobyla", corr="squared_exponential"),
+    GaussianProcess(regr="constant", optimizer="Welch", corr="squared_exponential"),
+    GaussianProcess(regr="linear", optimizer="fmin_cobyla", corr="squared_exponential"),
+    GaussianProcess(regr="linear", optimizer="Welch", corr="squared_exponential"),
+    GaussianProcess(regr="quadratic", optimizer="fmin_cobyla", corr="squared_exponential"),
+    GaussianProcess(regr="quadratic", optimizer="Welch", corr="squared_exponential"),
+    GaussianProcess(regr="constant", optimizer="fmin_cobyla", corr="generalized_exponential"),
+    GaussianProcess(regr="constant", optimizer="Welch", corr="generalized_exponential"),
+    GaussianProcess(regr="linear", optimizer="fmin_cobyla", corr="generalized_exponential"),
+    GaussianProcess(regr="linear", optimizer="Welch", corr="generalized_exponential"),
+    GaussianProcess(regr="quadratic", optimizer="fmin_cobyla", corr="generalized_exponential"),
+    GaussianProcess(regr="quadratic", optimizer="Welch", corr="generalized_exponential"),
+    GaussianProcess(regr="constant", optimizer="fmin_cobyla", corr="cubic"),
+    GaussianProcess(regr="constant", optimizer="Welch", corr="cubic"),
+    GaussianProcess(regr="linear", optimizer="fmin_cobyla", corr="cubic"),
+    GaussianProcess(regr="linear", optimizer="Welch", corr="cubic"),
+    GaussianProcess(regr="quadratic", optimizer="fmin_cobyla", corr="cubic"),
+    GaussianProcess(regr="quadratic", optimizer="Welch", corr="cubic"),
+    GaussianProcess(regr="constant", optimizer="fmin_cobyla", corr="linear"),
+    GaussianProcess(regr="constant", optimizer="Welch", corr="linear"),
+    GaussianProcess(regr="linear", optimizer="fmin_cobyla", corr="linear"),
+    GaussianProcess(regr="linear", optimizer="Welch", corr="linear"),
+    GaussianProcess(regr="quadratic", optimizer="fmin_cobyla", corr="linear"),
+    GaussianProcess(regr="quadratic", optimizer="Welch", corr="linear"),
+    GaussianNB(), MultinomialNB(), BernoulliNB(),
+    DecisionTreeClassifier(),
+    GradientBoostingClassifier(n_estimators=150, learning_rate=1.0, max_depth=1, random_state=0),
+    AdaBoostClassifier(n_estimators=150),
+]
+
+classifier_names = [
+    'LinearRegression()',
+    'RidgeCV(alphas=[.1, .5, 1, 5, 10])',
+    'LassoCV()',
+    'ElasticNetCV()',
+    'MultiTaskLassoCV()',
+    'BayesianRidge()',
+    'ARDRegression()',
+    'LogisticRegressionCV()',
+    'SGDClassifier(loss="hinge")',
+    'SGDClassifier(loss="log")',
+    'SGDClassifier(loss="modified_huber")',
+    'SGDClassifier(loss="squared_hinge")',
+    'SGDClassifier(loss="perceptron")',
+    'SGDClassifier(loss="squared_loss")',
+    'SGDClassifier(loss="huber")',
+    'SGDClassifier(loss="epsilon_insensitive")',
+    'SGDClassifier(loss="squared_epsilon_insensitive")',
+    'Perceptron()',
+    'PassiveAggressiveClassifier(loss="hinge")',
+    'PassiveAggressiveClassifier(loss="squared_hinge")',
+    'LinearDiscriminantAnalysis()',
+    'LinearDiscriminantAnalysis(solver="lsqr")',
+    'LinearDiscriminantAnalysis(solver="eigen")',
+    'SVC(kernel="poly")',
+    'SVC(kernel="sigmoid")',
+    'SVC(kernel="precomputed")',
+    'NuSVC()',
+    'LinearSVC()',
+    'KNeighborsClassifier(n_neighbors=8, weights="uniform")',
+    'KNeighborsClassifier(n_neighbors=8, weights="distance")',
+    'RadiusNeighborsClassifier(weights="uniform")',
+    'RadiusNeighborsClassifier(weights="distance")',
+    'GaussianProcess(regr="constant", optimizer="fmin_cobyla", corr="absolute_exponential")',
+    'GaussianProcess(regr="constant", optimizer="Welch", corr="absolute_exponential")',
+    'GaussianProcess(regr="linear", optimizer="fmin_cobyla", corr="absolute_exponential")',
+    'GaussianProcess(regr="linear", optimizer="Welch", corr="absolute_exponential")',
+    'GaussianProcess(regr="quadratic", optimizer="fmin_cobyla", corr="absolute_exponential")',
+    'GaussianProcess(regr="quadratic", optimizer="Welch", corr="absolute_exponential")',
+    'GaussianProcess(regr="constant", optimizer="fmin_cobyla", corr="squared_exponential")',
+    'GaussianProcess(regr="constant", optimizer="Welch", corr="squared_exponential")',
+    'GaussianProcess(regr="linear", optimizer="fmin_cobyla", corr="squared_exponential")',
+    'GaussianProcess(regr="linear", optimizer="Welch", corr="squared_exponential")',
+    'GaussianProcess(regr="quadratic", optimizer="fmin_cobyla", corr="squared_exponential")',
+    'GaussianProcess(regr="quadratic", optimizer="Welch", corr="squared_exponential")',
+    'GaussianProcess(regr="constant", optimizer="fmin_cobyla", corr="generalized_exponential")',
+    'GaussianProcess(regr="constant", optimizer="Welch", corr="generalized_exponential")',
+    'GaussianProcess(regr="linear", optimizer="fmin_cobyla", corr="generalized_exponential")',
+    'GaussianProcess(regr="linear", optimizer="Welch", corr="generalized_exponential")',
+    'GaussianProcess(regr="quadratic", optimizer="fmin_cobyla", corr="generalized_exponential")',
+    'GaussianProcess(regr="quadratic", optimizer="Welch", corr="generalized_exponential")',
+    'GaussianProcess(regr="constant", optimizer="fmin_cobyla", corr="cubic")',
+    'GaussianProcess(regr="constant", optimizer="Welch", corr="cubic")',
+    'GaussianProcess(regr="linear", optimizer="fmin_cobyla", corr="cubic")',
+    'GaussianProcess(regr="linear", optimizer="Welch", corr="cubic")',
+    'GaussianProcess(regr="quadratic", optimizer="fmin_cobyla", corr="cubic")',
+    'GaussianProcess(regr="quadratic", optimizer="Welch", corr="cubic")',
+    'GaussianProcess(regr="constant", optimizer="fmin_cobyla", corr="linear")',
+    'GaussianProcess(regr="constant", optimizer="Welch", corr="linear")',
+    'GaussianProcess(regr="linear", optimizer="fmin_cobyla", corr="linear")',
+    'GaussianProcess(regr="linear", optimizer="Welch", corr="linear")',
+    'GaussianProcess(regr="quadratic", optimizer="fmin_cobyla", corr="linear")',
+    'GaussianProcess(regr="quadratic", optimizer="Welch", corr="linear")',
+    'GaussianNB(), MultinomialNB(), BernoulliNB()',
+    'DecisionTreeClassifier()',
+    'GradientBoostingClassifier(n_estimators=150, learning_rate=1.0, max_depth=1, random_state=0)',
+    'AdaBoostClassifier(n_estimators=150)',
+]
+
+
+def main(classifier_id=None, training_factor=0.5):
+    data = Data(training_factor)
+    vectorizer = DataVectorizer()
+    corpora = vectorizer.convert(data.get_training_data(), data.get_test_data())
+    if classifier_id is None:
+        for index in range(0, len(classifiers)):
+            run_classifier_with_id(index, corpora['training'], corpora['test'])
+    else:
+        run_classifier_with_id(classifier_id, corpora['training'], corpora['test'])
+
+
+def run_classifier_with_id(classifier_id, training_corpus, test_corpus):
+    executor = ClassifierExecutor(classifiers[classifier_id])
+    try:
+        print "%s: %.4f" % (classifier_names[classifier_id], executor.execute(training_corpus, test_corpus))
+    except Exception as e:
+        print "%s: %s" % (classifier_names[classifier_id], e)
+
+
+main(1)
