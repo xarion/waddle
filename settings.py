1b3901cc9ffd61bbdd58ef1b77a9394101c6d477
xarion <erdicalli@gmail.com>
2016-01-06 11:39:56 +0100

    added top locations to use to settings

--- settings.py
+++ settings.py
@@ -17,3 +17,12 @@ mongod_config['database_name'] = configParser.get('mongod', 'database_name')
 streaming_config = dict()
 streaming_config['chunk_size'] = int(configParser.get('streaming', 'chunk_size'))
 streaming_config['total_chunks'] = int(configParser.get('streaming', 'total_chunks'))
+
+locations = {"Manhattan, NY": 1,
+             "Los Angeles, CA": 2,
+             "Houston, TX": 3,
+             "Chicago, IL": 4,
+             "San Francisco, CA": 5,
+             "Philadelphia, PA": 6,
+             "Washington, DC": 7,
+             "Atlanta, GA": 8}
