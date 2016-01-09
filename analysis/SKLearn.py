d82681903df94f1623bac504c8a6f89280b6dc4b
xarion <erdicalli@gmail.com>
2016-01-09 12:22:50 +0100

    normalised data by fixing the number of documents per class

--- analysis/SKLearn.py
+++ analysis/SKLearn.py
@@ -36,6 +36,43 @@ class DataVectorizer:
         test_corpus['tfidfs'] = self.tfidf_transformer.transform(self.vectorizer.transform(test_corpus['documents']))
         return {"training": training_corpus, "test": test_corpus}
 
+    def normalise(self, docs):
+        normalised_docs = {"document_labels": [], "documents": []}
+        if self.binary_classification:
+            positives = sum(docs["document_labels"])
+            negatives = len(docs["document_labels"]) - positives
+            positive_count = 0
+            negative_count = 0
+            limit = positives if positives < negatives else negatives
+            count = 0
+            while positive_count < limit or negative_count < limit:
+                label = docs["document_labels"][count]
+                positive_count += label
+                negative_count += not label
+                if (label and positive_count < limit) or (not label and negative_count < limit):
+                    normalised_docs["document_labels"].append(label)
+                    normalised_docs["documents"].append(docs["documents"][count])
+                count += 1
+        else:
+            location_counts = Dictrement()
+            for label in docs["document_labels"]:
+                location_counts.increment(label)
+            limit = sys.maxint
+            for label in location_counts.underlying.keys():
+                if location_counts.get(label) < limit:
+                    limit = location_counts.get(label)
+            count = 0
+            number_of_docs = len(docs["document_labels"])
+            new_counts = Dictrement()
+            while count < number_of_docs:
+                label = docs["document_labels"][count]
+                if new_counts.get(label) < limit:
+                    normalised_docs["document_labels"].append(label)
+                    normalised_docs["documents"].append(docs["documents"][count])
+                    new_counts.increment(label)
+                count += 1
+
+        return normalised_docs
     @staticmethod
     def __create_corpus__(docs):
         documents = []
