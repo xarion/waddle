cc72d43220e7e45d7f0cb7f7c3153d6057834ef8
xarion <erdicalli@gmail.com>
2016-01-12 18:24:13 +0100

    moved main into a class.

--- sklearn_comparison.py
+++ sklearn_comparison.py
@@ -2,10 +2,9 @@ import sys
 from datetime import datetime
 
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.linear_model import LogisticRegressionCV, SGDClassifier, RidgeCV, LinearRegression, LassoCV, ElasticNetCV, \
-    Perceptron, PassiveAggressiveClassifier
+from sklearn.linear_model import LogisticRegressionCV, SGDClassifier, Perceptron, PassiveAggressiveClassifier
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
-from sklearn.neighbors import KNeighborsClassifier, RadiusNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC, LinearSVC
 from sklearn.tree import DecisionTreeClassifier
 
@@ -79,60 +78,64 @@ execution = Execution()
 instance_meta = {}
 
 
-def create_corpus(configuration):
-    return Corpus(binary_classification=configuration[0], include_user_history=configuration[1],
-                  ngram=configuration[2])
+class Main:
+    def __init__(self):
+        self.instance_meta = {}
 
+    @staticmethod
+    def create_corpus(configuration):
+        return Corpus(binary_classification=configuration[0], include_user_history=configuration[1],
+                      ngram=configuration[2])
 
-def main(classifier=None, configuration_id=0):
-    if classifier is None:
-        for index in range(0, len(classifiers)):
-            run_classifier_with_id(index, configuration_id)
-    elif classifier == "all-gcloud":
-        super.instance_meta = gcloud.get_metadata()
-        run_after_progress()
-    else:
-        run_classifier_with_id(int(classifier), configuration_id)
+    def start(self, classifier=None, configuration_id=0):
+        if classifier is None:
+            for index in range(0, len(classifiers)):
+                self.run_classifier_with_id(index, configuration_id)
+        elif classifier == "all-gcloud":
+            # self.instance_meta = gcloud.get_metadata()
+            self.run_after_progress()
+        else:
+            self.run_classifier_with_id(int(classifier), configuration_id)
 
+    def run_classifier_with_id(self, classifier_id, configuration_id):
+        configuration = configurations[configuration_id]
+        result = {"classifier": classifier_names[classifier_id], "classifier_id": classifier_id,
+                  "configuration_id": configuration_id, "binary_classification": configuration[0],
+                  "include_user_history": configuration[1], "ngram": configuration[2], "started_at": datetime.now(),
+                  "meta": instance_meta}
+        execution.write_progress(result)
+        corpus = self.create_corpus(configuration)
+        executor = ClassifierExecutor(classifiers[classifier_id], binary_classification=configuration[0])
+        try:
+            result['result'] = executor.execute(corpus.get_training(), corpus.get_test())
+            print "%s\n%s" % (classifier_names[classifier_id], str(result["result"]))
+        except Exception as e:
+            print "%s: %s" % (classifier_names[classifier_id], e)
+            result['exception'] = str(sys.exc_info())
+        result['ended_at'] = datetime.now()
+        result['execution_time'] = str(result['ended_at'] - result['started_at'])
+        execution.write_execution_result(result)
 
-def run_classifier_with_id(classifier_id, configuration_id):
-    configuration = configurations[configuration_id]
-    result = {"classifier": classifier_names[classifier_id], "classifier_id": classifier_id,
-              "configuration_id": configuration_id, "binary_classification": configuration[0],
-              "include_user_history": configuration[1], "ngram": configuration[2], "started_at": datetime.now(),
-              "meta": instance_meta}
-    execution.write_progress(result)
-    corpus = create_corpus(configuration)
-    executor = ClassifierExecutor(classifiers[classifier_id], binary_classification=configuration[0])
-    try:
-        result['result'] = executor.execute(corpus.get_training(), corpus.get_test())
-        print "%s\n%s" % (classifier_names[classifier_id], str(result["result"]))
-    except Exception as e:
-        print "%s: %s" % (classifier_names[classifier_id], e)
-        result['exception'] = str(sys.exc_info())
-    result['ended_at'] = datetime.now()
-    result['execution_time'] = str(result['ended_at'] - result['started_at'])
-    execution.write_execution_result(result)
-
-
-def run_after_progress():
-    progress = execution.get_last_progress()
-    if progress is None:
-        run_classifier_with_id(0, 0)
-        return run_after_progress()
-    elif progress["configuration_id"] + 1 < len(configurations):
-        run_classifier_with_id(progress["classifier_id"], progress["configuration_id"] + 1)
-    elif progress["classifier_id"] + 1 < len(classifiers):
-        run_classifier_with_id(progress["classifier_id"] + 1, 0)
-        return run_after_progress()
-    else:
-        return 0
+    def run_after_progress(self):
+        progress = execution.get_last_progress()
+        if progress is None:
+            self.run_classifier_with_id(0, 0)
+            return self.run_after_progress()
+        elif progress["configuration_id"] + 1 < len(configurations):
+            self.run_classifier_with_id(progress["classifier_id"], progress["configuration_id"] + 1)
+            return self.run_after_progress()
+        elif progress["classifier_id"] + 1 < len(classifiers):
+            self.run_classifier_with_id(progress["classifier_id"] + 1, 0)
+            return self.run_after_progress()
+        else:
+            return 0
 
 
 if __name__ == "__main__":
+    m = Main()
     if len(sys.argv) > 2:
-        main(sys.argv[1], sys.argv[2])
+        m.start(sys.argv[1], sys.argv[2])
     elif len(sys.argv) > 1:
-        main(sys.argv[1])
+        m.start(sys.argv[1])
     else:
-        main()
+        m.start()
